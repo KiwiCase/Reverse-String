3afefb19c17a6895ac7dedb5e4c9112a17cef9d1
Kiwicase <emailcaseydent@gmail.com>
2022-07-18 18:04:55 +0300

    Reverse String - code added for checking for palemdromes

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -11,8 +11,16 @@ namespace ReverseString
             char[] stringArray = ogString.ToCharArray();
             Array.Reverse(stringArray);
             string revString = new string(stringArray);
+            bool b = ogString.Equals(revString, StringComparison.OrdinalIgnoreCase);
+            if (b == true)
+            {
+                Console.WriteLine("" + ogString + " is a Palindrome!");
+            } else
+            {
+                Console.WriteLine($"Your string reversed = {revString}");
+            }
 
-            Console.WriteLine($"Your string reversed = {revString}");
+            
         }
     }
 }
