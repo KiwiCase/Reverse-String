e14c25632d3fccd430671eef636cd0e5e7a10de7
Kiwicase <emailcaseydent@gmail.com>
2022-07-23 18:28:59 +0300

    Reverse String - string accessor, beginning of implementing.

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -5,22 +5,12 @@ namespace ReverseString
     internal class Program
     {
         static void Main(string[] args)
+
         {
             Console.WriteLine("Enter your string: ");
-            string ogString = Console.ReadLine();
-            char[] stringArray = ogString.ToCharArray();
-            Array.Reverse(stringArray);
-            string revString = new string(stringArray);
-            bool b = ogString.Equals(revString, StringComparison.OrdinalIgnoreCase);
-            if (b == true)
-            {
-                Console.WriteLine($"{ogString} is a Palindrome!");
-            }
-            else
-            {
-                Console.WriteLine($"Your string reversed = {revString}");
-            }
+            string inputString = Console.ReadLine();
 
+            Console.WriteLine(inputString[10]); Console.WriteLine(inputString[9]);
 
         }
     }
